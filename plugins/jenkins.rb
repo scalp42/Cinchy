5b796976cc326cfcba48b3a40fd69bd0f4b95334
Callum Jones <callum@callumj.com>
2012-01-10 18:17:08 +0800

    Why did I use 400, instead of 200?

--- plugins/jenkins.rb
+++ plugins/jenkins.rb
@@ -20,7 +20,7 @@ module BotPlugins
           job = match_data[1]
           build_key = config.plugins.jenkins.keys.send(job.downcase)
           response = HTTParty.get("#{base_addr}/job/#{job}/build?token=#{build_key}&cause=IRC+command+from+#{m.user.nick}", options)
-          m.reply("Kicking off build") if response.code.eql?(400)
+          m.reply("Kicking off build") if response.code.eql?(200)
         end
       else
         # just show an update
