cb046b7ffa95e3c8b5f180eecbf4b1851ff04527
yoda <yoda@tallon4.net>
2012-01-16 15:38:03 +0800

    Adding quick_list

---
+++ plugins/quick_list.rb
@@ -0,0 +1,23 @@
+require 'pathname'
+require 'uri'
+require 'find'
+module BotPlugins
+  class QuickList
+    include Cinch::Plugin
+
+    match /list (.+)/
+  
+    help "Shows available files to be played via VLC"
+    
+    def execute(m, params)
+        config = HelperLib::SharedObject.instance(:config)
+        start_path = config.plugins.music_path
+      
+        m.reply("Available files in: #{start_path}")
+        Find.find(start_path) do |file_path|
+          m.reply("#{file_path}")
+        end
+      
+    end
+  end
+end
